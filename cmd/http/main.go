47f352712871dbd3f2d5feb71f6d2370974c0d89
Veit Osiander <blockchain@veitosiander.de>
2017-12-09 22:38:06 +0100

    add basic json http response

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/siklol/blockchain/handler"
+)
 
 func main() {
+
+	// router
 	router := gin.Default()
 
-	router.Run()
+	// handlers
+	handler.NewDefaultHandler().Register(router)
+
+	if err := router.Run(); err != nil {
+		log.Fatalf("fatal error occured: %s", err.Error())
+	}
 }
--- handler/default.go
+++ handler/default.go
@@ -1,8 +1,20 @@
 package handler
 
+import "github.com/gin-gonic/gin"
+
 type defaultHandler struct {
 }
 
 func NewDefaultHandler() *defaultHandler {
 	return &defaultHandler{}
+}
+
+func (h *defaultHandler) Register(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) { h.GetDefault(c) })
+}
+
+func (h *defaultHandler) GetDefault(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world!",
+	})
 }
